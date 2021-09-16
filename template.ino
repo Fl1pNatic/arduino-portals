086026040101f30c969337df55d3403f6a145110
Fl1p <41564630+Fl1pNatic@users.noreply.github.com>
2021-09-16 20:19:22 +0600

    template

---
+++ template.ino
@@ -0,0 +1,16 @@
+#include <OLED_I2C.h>
+OLED  screen(SDA, SCL, 8);
+extern uint8_t SmallFont[];
+void setup() {
+  //Sets pinmodes for 4 buttons that I have
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);      
+}
+
+void loop() {
+  screen.clrScr();
+  //Code here
+  screen.update();
+}
